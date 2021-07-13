80be2d32827bc698f2e8ce124fb5ecad43fd7084
Shah Wajahat <shahwjahatijaz@gmail.com>
2021-07-14 00:31:23 +0500

    Working views.py creating get ,put and delete request for specific data

--- countries/views.py
+++ countries/views.py
@@ -1,4 +1,5 @@
 import re
+from django.core.checks import messages
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
@@ -29,3 +30,23 @@ def countries_list(request):
             countires_serializer.save()
             return JsonResponse(countires_serializer.data,status=status.HTTP_201_CREATED)
         return JsonResponse(countires_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET","PUT","DELETE"])
+def countries_detail(request,pk):
+    try:
+        countries=Countries.objects.get(pk=pk)
+    except Countries.DoesNotExist:
+        return JsonResponse({'message':'The country does not exist'},status=status.HTTP_404_NOT_FOUND)
+    if request.method =="GET":
+        countries_serializer=CountriesSerializer(countries)
+        return JsonResponse(countries_serializer.data,status=status.HTTP_200_OK)
+    elif request.method=="PUT":
+        countries_data=JSONParser.parse(request)
+        countries_serializer=CountriesSerializer(countries,data=countries_data)
+        if countries_serializer.is_valid():
+            countries_serializer.save()
+            return JsonResponse(countries_serializer.data,status=status.HTTP_200_OK)
+        return JsonResponse(countries_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        countries.delete()
+        return JSONParser({"message":"Country was deleted succesfully"},status=status.HTTP_204_NO_CONTENT)
