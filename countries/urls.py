25853ce667876e0609656554806f7d8bcb064c62
Shah Wajahat <shahwjahatijaz@gmail.com>
2021-07-14 01:05:51 +0500

    Error remove from urls.py

--- countries/urls.py
+++ countries/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.urls.resolvers import URLPattern
+from django.conf.urls import url
 from countries import views
 
-urlpattern=[
+urlpatterns=[
     url(r'^api/countries$',views.countries_list),
     url(r'^api/countries/(?P<pk>[0-9]+)$',views.countries_detail)
 ]
--- worldCountries/urls.py
+++ worldCountries/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import url , include
+from django.conf.urls import url,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^',include('countries.urls')),
+    url(r'',include('countries.urls')),
 ]
